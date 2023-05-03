6000293d6360ab90c60b1f27f24d513184f709ac
Chaedonghyun <ehd3874@gmail.com>
2023-05-03 16:57:46 +0900

    feat: initialize project

---
+++ chatting/chatting.cpp
@@ -0,0 +1,282 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h> 
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <thread>
+#include <mysql/jdbc.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "abc1234";
+
+
+#define MAX_SIZE 1024
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+sql::mysql::MySQL_Driver* driver;
+sql::Connection* con;
+sql::Statement* stmt;
+sql::PreparedStatement* pstmt;
+sql::ResultSet* result;
+
+
+SOCKET client_sock;
+string id_in;
+
+int chat_recv() {
+    char buf[MAX_SIZE] = { };
+    string msg;
+
+    while (1) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);
+            string user;
+            ss >> user;
+            if (user != id_in) cout << buf << endl;
+        }
+        else {
+            cout << "Server Off" << endl;
+            return -1;
+        }
+    }
+}
+
+void SignUp() {
+    string id, pw, name;
+    string check_id, check_pw;
+    while (1) {
+        cout << "사용할 아이디를 입력하세요:";
+        cin >> id;
+
+        pstmt = con->prepareStatement("SELECT * FROM user where id=? ;");
+        pstmt->setString(1, id);
+        pstmt->execute();
+        result = pstmt->executeQuery();
+
+        while (result->next()) {
+            check_id = result->getString(1).c_str();
+        }
+        if (check_id == id) {
+            cout << "이미 사용중인 아이디 입니다." << endl;
+        }
+        else if (check_id != id) {
+            cout << "사용할 비밀번호를 입력하세요: ";
+            cin >> pw;
+            cout << "본인의 이름을 입력하세요: ";
+            cin >> name;
+            break;
+        }
+    }
+    pstmt = con->prepareStatement("insert into user(id, pw, user_name) values(?,?,?)");
+    pstmt->setString(1, id);
+    pstmt->setString(2, pw);
+    pstmt->setString(3, name);
+    pstmt->execute();
+
+    cout << "회원가입 성공" << endl;
+}
+
+void Store() {
+    string StoreUser, StoreMsg;
+
+    cout << "저장된 내용" << endl;
+    con->setSchema("project");
+    pstmt = con->prepareStatement("SELECT * FROM chatting;");
+    result = pstmt->executeQuery();
+
+    while (result->next()) {
+        StoreUser = result->getString("id");
+        StoreMsg = result->getString("chat");
+        cout << StoreUser << " : " << StoreMsg << endl;
+    }
+
+}
+
+void Savechatting() {
+    
+    
+
+}
+
+void Revise() {
+    string id, pw;
+    string check_id, check_pw;
+
+    cout << "아이디를 입력하세요. : ";
+    cin >> id;
+    cout << "비밀번호를 입력하세요";
+    cin >> pw;
+
+    pstmt = con->prepareStatement("SELECT * FROM user where id = ? and pw =?;");
+    pstmt->setString(1, id);
+    pstmt->setString(2, pw);
+    pstmt->execute();
+    result = pstmt->executeQuery();
+
+    while (result->next()) {
+        check_id = result->getString(1).c_str();
+        check_pw = result->getString(2).c_str();
+    }
+
+    if (check_id != id || check_pw!=pw) {
+        cout << "아이디,비밀번호가 맞지 않습니다.\n";
+    }
+
+    else {
+        cout << "변경할 비밀번호를 입력해 주세요. : ";
+        cin >> pw;
+        pstmt = con->prepareStatement("select id from user");
+        result = pstmt->executeQuery();
+
+        while (result->next()) {
+            pstmt = con->prepareStatement("UPDATE user SET pw = ? WHERE id = ?");
+            pstmt->setString(1, pw);
+            pstmt->setString(2, id);
+            pstmt->executeQuery();
+        }
+        cout << "변경 되었습니다\n";
+    }
+
+}
+
+
+
+int main()
+{
+    WSADATA wsa;
+    int choice = 0;
+    bool log = true;
+    int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    // sql 연결
+    try {
+        driver = sql::mysql::get_mysql_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException& e) {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        exit(1);
+    }
+
+    con->setSchema("project");
+
+    stmt = con->createStatement();
+    stmt->execute("set names euckr");
+    if (stmt) { delete stmt; stmt = nullptr; }
+
+    stmt = con->createStatement();
+    delete stmt;
+    
+    //createtable();
+
+
+    if (!code) {
+
+        client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        SOCKADDR_IN client_addr = {};
+        client_addr.sin_family = AF_INET;
+        client_addr.sin_port = htons(8080);
+        InetPton(AF_INET, TEXT("127.0.0.1"), &client_addr.sin_addr);
+        client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        while (true) {
+
+            cout << "1: 로그인하기 2: 회원가입하기 3: 비밀번호 수정" << endl;
+            cin >> choice;
+
+            // 로그인
+
+            if (choice == 1) {
+                string pw;
+                string check_id, check_pw;
+
+                cout << "ID:";
+                cin >> id_in;
+                cout << "PW:";
+                cin >> pw;
+
+                pstmt = con->prepareStatement("SELECT * FROM user where id=? and pw=?;");
+                pstmt->setString(1, id_in);
+                pstmt->setString(2, pw);
+                pstmt->execute();
+                result = pstmt->executeQuery();
+
+
+                while (result->next()) {
+                    check_id = result->getString(1).c_str();
+                    check_pw = result->getString(2).c_str();
+                }
+                if (check_id == id_in && check_pw == pw) {
+                    cout << "로그인 되었습니다." << endl;
+                    while (1) {
+                        while (1) {
+                            if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) {
+                                cout << "Server Connect" << endl;
+                                send(client_sock, id_in.c_str(), id_in.length(), 0);
+                                break;
+                            }
+                            cout << "Connecting..." << endl;
+                        }
+
+                        std::thread th2(chat_recv);
+
+                        while (1) {
+                            string text;
+                            std::getline(cin, text);
+                            const char* buffer = text.c_str(); // string형을 char* 타입으로 변환
+                            send(client_sock, buffer, strlen(buffer), 0);
+
+                            Store();
+
+                            pstmt = con->prepareStatement("insert into chatting(id, chat) values(?,?)");
+                            pstmt->setString(1, id_in);
+                            pstmt->setString(2, text);
+                            pstmt->execute();
+                            
+                            
+
+                        }
+                        th2.join();
+                        closesocket(client_sock);
+                    }
+                }
+                else {
+                    cout << "로그인에 실패했습니다." << endl;
+                    continue;
+                }
+
+
+
+            }
+            if (choice == 2)
+            {
+                SignUp();
+                continue;
+            }
+
+            if (choice == 3)
+            {
+                Revise();
+                continue;
+            }
+        }
+
+
+        WSACleanup();
+
+    }
+    //
+}
---
+++ server/server.cpp
@@ -0,0 +1,146 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#define MAX_SIZE 1024
+#define MAX_CLIENT 3
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+using std::vector;
+
+struct SOCKET_INFO
+{
+    SOCKET sck;
+    string user;
+};
+vector<SOCKET_INFO> sck_list;
+SOCKET_INFO server_sock;
+int client_count=0;
+
+void server_init();
+void add_client();
+void send_msg(const char* msg);
+void recv_msg(int idx);
+void del_client(int idx);
+
+
+
+int main()
+{
+    WSADATA wsa;
+
+    int code = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    if (!code)
+    {
+        server_init();
+        std::thread th1[MAX_CLIENT];
+        for (int i = 0; i < MAX_CLIENT; i++)
+        {
+            th1[i] = std::thread(add_client);
+        }
+
+        while (1)
+        {
+            string text, msg = "";
+
+            std::getline(cin, text);
+            const char* buf = text.c_str();
+            msg = server_sock.user + ":" + buf;
+            send_msg(msg.c_str());
+        }
+
+        for (int i = 0; i < MAX_CLIENT; i++)
+        {
+            th1[i].join();
+        }
+   
+    }
+    else
+    {
+        cout << "프로그램 종류. (Error Code:" << code << ")";
+    }
+
+    WSACleanup();
+
+}
+
+void server_init() {
+    server_sock.sck = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    SOCKADDR_IN server_addr = {};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bind(server_sock.sck, (sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server_sock.sck, SOMAXCONN);
+    server_sock.user = "server";
+    cout << "open" << endl;
+}
+
+void add_client() {
+    SOCKADDR_IN addr = {};
+    int addrsize = sizeof(addr);
+    char buf[MAX_SIZE] = { };
+
+    ZeroMemory(&addr, addrsize);
+    SOCKET_INFO new_client = {};
+    new_client.sck = accept(server_sock.sck, (sockaddr*)&addr, &addrsize);
+    recv(new_client.sck,buf, MAX_SIZE, 0);
+    new_client.user = string(buf);
+
+    string msg = new_client.user + "님이 입장했습니다";
+    cout << msg << endl;
+    sck_list.push_back(new_client);
+
+    std::thread th(recv_msg, client_count);
+
+    client_count++;
+    cout << "현재 인원수:" << client_count << "명" << endl;
+    send_msg(msg.c_str());
+    th.join();
+}
+
+void send_msg(const char* msg) {
+    for (int i = 0; i < client_count; i++)
+    {
+        send(sck_list[i].sck, msg, MAX_SIZE, 0);
+    }
+}
+
+void recv_msg(int idx) {
+    char buf[MAX_SIZE] = { };
+    string msg = "";
+    while (1)
+    {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(sck_list[idx].sck,buf,MAX_SIZE,0)>0)
+        {
+            msg = sck_list[idx].user + ":" + buf;
+            cout << msg << endl;
+            send_msg(msg.c_str());
+        }
+        else
+        {
+            msg = sck_list[idx].user + "님이 퇴장했습니다";
+            cout << msg << endl;
+            send_msg(msg.c_str());
+            del_client(idx);
+            return;
+        }
+    }
+
+}
+
+void del_client(int idx) {
+    closesocket(sck_list[idx].sck);
+    client_count--;
+}
